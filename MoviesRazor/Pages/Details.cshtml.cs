ff424e404a896b03c494d92f9567ae4925b9f65b
sergey <sergeysereko@gmail.com>
2023-10-11 17:37:48 +0300

    task#4 Добавил страницы Edit и Details

---
+++ MoviesRazor/Pages/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MoviesRazor.Models;
+
+namespace MoviesRazor.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MoviesRazor.Models.MovieContext _context;
+
+        public DetailsModel(MoviesRazor.Models.MovieContext context)
+        {
+            _context = context;
+        }
+
+      public Movie Movie { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
