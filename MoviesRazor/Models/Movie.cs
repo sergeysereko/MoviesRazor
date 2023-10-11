eea88b56c1ca815f0ff39ffac76f4a4a2f6d9606
sergey <sergeysereko@gmail.com>
2023-10-11 16:35:00 +0300

    task #1 Добавил модель Movie

---
+++ MoviesRazor/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿namespace MoviesRazor.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Director { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Year { get; set; }
+
+        public string? Poster { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
